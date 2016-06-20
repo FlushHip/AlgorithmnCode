7520784ad1b53199f7a79c57088f7e8227ceb1a3
FlushHip <chenyangweixiao@qq.com>
2016-06-20 19:05:04 +0800

    17547818, WA

--- UVa11078.cpp
+++ UVa11078.cpp
@@ -13,11 +13,11 @@ int main()
 		for(int i=0;i<n;i++)
 			scanf("%d",&a[i]);
 		int tmpmax=a[0];
-		int ans=-150000+6;
+		int ans=-150000*2-6;
 		for(int i=1;i<n;i++)
 			ans=MAX(ans,tmpmax-a[i]),
 			tmpmax=MAX(tmpmax,a[i]);
 		printf("%d\n",ans);
 	}
-	return 0;	
+	return 0;
 }
