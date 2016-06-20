e4b87cfa03c9f06dad8b5913c7f5721b4dbc593a
FlushHip <chenyangweixiao@qq.com>
2016-06-20 17:53:48 +0800

    17547542, WA

---
+++ UVa11078.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MAX(a,b) ((a)>(b)? (a):(b))
+
+const int M=100000+5;
+
+int n;
+int a[M];
+
+int main()
+{
+	while(~scanf("%d",&n)){
+		for(int i=0;i<n;i++)
+			scanf("%d",&a[i]);
+		int tmpmax=a[0];
+		int ans=-150000+6;
+		for(int i=1;i<n;i++)
+			ans=MAX(ans,tmpmax-a[i]),
+			tmpmax=MAX(tmpmax,a[i]);
+		printf("%d\n",ans);
+	}
+	return 0;	
+}
