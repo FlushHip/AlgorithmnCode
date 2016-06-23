8e7023530a552e20e6267fd848d24e3d4648ef7e
ChenYang <chenyangweixiao@qq.com>
2016-06-23 15:11:07 +0800

    Create the file

---
+++ POJ2253.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX(a,b) ((a)>(b)? (a):(b))
+#define MIN(a,b) ((a)<(b)? (a):(b))
+
+const int M=200+5;
+
+const int INF=0x3f3f3f3f;
+
+typedef struct tagPoint{
+	int x,y;
+}Point;
+
+int n;
+
+Point points[M];
+
+double map[M][M];
+
+bool used[M];
+double dis[M];
+
+double dis2(Point a,Point b)
+{
+	return (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y);
+}
+
+double dij(int src)
+{
+	double ans=0;
+	int i,j;
+	for(i=1;i<=n;i++)
+		dis[i]=INF,used[i]=false;
+	dis[src]=0;
+	for(i=1;i<=n;i++){
+		int pos=0;
+		double tmp=INF;
+		for(j=1;j<=n;j++)
+			if(!used[j]&&tmp>dis[j])
+				pos=j,tmp=dis[j];
+		used[pos]=true;
+		for(j=1;j<=n;j++)
+			if(!used[j]){
+				ans=MAX(ans,map[pos][j]);
+				dis[j]=MIN(dis[pos]+map[pos][j],dis[j]);
+			}
+	}
+}
+
+int main()
+{
+	int K=1;
+	while(scanf("%d",&n),n){
+		int i,j;
+		int x,y;
+		for(i=1;i<=n;i++){
+			scanf("%d%d",&x,&y);
+			points[i]=Node(x,y);
+		}
+		for(i=1;i<=n;i++)
+			for(j=i;j<=n;j++)
+				map[i][j]=map[j][i]=dis2(points[i],points[j]);
+
+		double ans=sqrt(dij(1));
+		printf("Scenario #%d\nFrog Distance = %.3f\n\n",K++,ans);
+	}
+	return 0;
+}
