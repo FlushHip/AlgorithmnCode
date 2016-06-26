8a078f43f88c85a307572dbd7d560555dc5d57f7
FlushHip <chenyangweixiao@qq.com>
2016-06-27 01:57:48 +0800

    can't pass sample input

--- UVa11624.java
+++ UVa11624.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class UVa11624 {
 	Scanner cin = new Scanner(System.in);
 
 	static final int M = 1000 + 25;
@@ -26,9 +26,10 @@ public class Main {
 			int tx = x + dirx[i];
 			int ty = y + diry[i];
 			if (tx >= 1 && tx <= R && ty >= 1 && ty <= C && !used[tx][ty]
-					&& map[tx][ty] != '#')
+					&& map[tx][ty] != '#') {
 				used[tx][ty] = true;
-			dfs_init(tx, ty, sum + 1);
+				dfs_init(tx, ty, sum + 1);
+			}
 		}
 	}
 
@@ -41,17 +42,21 @@ public class Main {
 				if (map[i][j] == 'F') {
 					for (int x = 1; x <= R; x++)
 						for (int y = 1; y <= C; y++)
-							used[i][j] = false;
+							used[x][y] = false;
+					used[i][j] = true;
 					dfs_init(i, j, 0);
 				}
 	}
 
 	int bfs() {
+		if(X == 1 || X == R || Y == 1 || Y == C)
+			return 1;
 		for (int i = 1; i <= R; i++)
 			for (int j = 1; j <= C; j++)
 				used[i][j] = false;
 		head = tail = 0;
 		que[tail++] = new Node(X, Y, 0);
+		used[X][Y] = true;
 		tail %= INF;
 		while (head != tail) {
 			Node pre = que[head++];
@@ -61,16 +66,14 @@ public class Main {
 				int ty = pre.y + diry[i];
 				Node now = new Node(tx, ty, pre.step + 1);
 				if (tx >= 1 && tx <= R && ty >= 1 && ty <= C
-						&& map[tx][ty] != '#') {
-					if (now.step < fireTime[tx][ty])
+						&& map[tx][ty] != '#' && !used[tx][ty]) {
+					if (now.step >= fireTime[tx][ty])
 						continue;
 					if (tx == 1 || tx == R || ty == 1 || ty == C)
-						return now.step;
-					if (!used[tx][ty]) {
-						used[tx][ty] = true;
-						que[tail++] = now;
-						tail %= INF;
-					}
+						return now.step + 1;
+					used[tx][ty] = true;
+					que[tail++] = now;
+					tail %= INF;
 
 				}
 			}
@@ -78,7 +81,7 @@ public class Main {
 		return -1;
 	}
 
-	Main() {
+	UVa11624() {
 		int N = cin.nextInt();
 		while (N-- > 0) {
 			R = cin.nextInt();
@@ -104,7 +107,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		new Main();
+		new UVa11624();
 	}
 }
 
