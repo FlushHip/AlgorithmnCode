e5f024bd08594a68b501f99eae8b2564beca9cea
ChenYang <chenyangweixiao@qq.com>
2018-02-27 09:35:15 +0800

    AC

--- 今日头条2018校招后端方向(第二批)第一题.cpp
+++ 今日头条2018校招后端方向(第二批)第一题.cpp
@@ -16,19 +16,19 @@ struct find_first_cmp {
 
 	bool operator()(int k, const pair<int, int> &P) const
 	{
-		return k > P.first;
+		return k < P.first;
 	}
 };
 
 struct find_second_cmp {
 	bool operator()(const pair<int, int> &P, int k) const
 	{
-		return P.second <= k;
+		return P.second < k;
 	}
 
 	bool operator()(int k, const pair<int, int> &P) const
 	{
-		return k >= P.second;
+		return k < P.second;
 	}
 };
 
@@ -37,16 +37,16 @@ int main()
 	int n, q;
 	while (EOF != scanf("%d", &n)) {
 		vector<pair<int, int> > arr;
-		for (int i = 0, x; i < n; cin >> x, arr.emplace_back(x, i)) {}
+		for (int i = 0, x; i < n; cin >> x, arr.emplace_back(x, ++i)) {}
 		sort(arr.begin(), arr.end(), sort_cmp);
 
 		for (scanf("%d", &q); q--;) {
 			int L, R, k;
 			scanf("%d%d%d", &L, &R, &k);
 			pair<vector<pair<int, int> >::iterator, vector<pair<int, int> >::iterator> sd =
-				equal_range(arr.begin(), arr.end(), k, find_first_cmp());
-        		printf("%d\n", upper_bound(sd.first, sd.second, L, find_second_cmp()) -
-				 lower_bound(sd.first, sd.second, R, find_second_cmp()));
+				equal_range(arr.begin(), arr.end(), k, find_first_cmp{});
+			printf("%d\n", upper_bound(sd.first, sd.second, R, find_second_cmp{}) -
+				lower_bound(sd.first, sd.second, L, find_second_cmp{}));
 		}
 	}
 	return 0;
